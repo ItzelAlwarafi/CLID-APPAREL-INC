51340d866d6dc639b26f95611464a72c31d1413f
CV2Tre <cvlookin@me.com>
2024-02-13 18:54:34 -0500

    adding controller

---
+++ controllers/clothesController.js
@@ -0,0 +1,71 @@
+const { Clothes} = require('../models')
+
+const getAllClothes = async (req, res) => {
+   try {
+      const clothes = await Clothes.find({})
+      res.json(clothes)
+   } catch (e) {
+      return res.status(500).send(e.message)
+   }
+}
+
+const getOneClothes = async (req, res) => {
+   try {
+      const { title } = req.params
+      const clothes = await Clothes.findOne({title: title})
+      .populate('clothes')
+      .exec()
+      console.log(clothes)
+      if (clothes) {
+         return res.json(clothes)
+        }
+        return res.status(404).send('Recipe cannot be found')
+     } catch (e) {
+        return res.status(500).send(e.message)
+     }
+  }
+
+  const createClothes = async (req, res) => {
+   try{
+   const clothes = await Clothes.create(req.body)
+   return res.status(201).json({
+      recipe,
+   })
+   } catch (e) {
+      return res.status(500).json({ error: e.message })
+   }
+}
+
+const updateClothes = async (req, res) => {
+   try {
+      let id = req.params.id
+      let clothes = await Movie.findByIdAndUpdate(id, req.body, { new: true })
+      if (clothes) {
+         return res.status(200).json(movie)
+      }
+      throw new Error('Recipe not found')
+   } catch (e) {
+      return res.status(500).send(e.message)
+   }
+}
+
+const deleteClothes = async (req, res) => {
+   try {
+      const id = req.params.id
+      let clothes = await Clothes.findByIdAndDelete(id)
+      if (clothes) {
+         return res.status(200).json(movie)
+      }
+      throw new Error('Clothes not found')
+   } catch (e) {
+      return res.status(500).send(e.message)
+   }
+}
+  
+  module.exports = {
+   getAllClothes,
+  getOneClothes,
+  createClothes,
+  updateClothes,
+  deleteClothes
+  }
